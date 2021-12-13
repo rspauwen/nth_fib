db56909ff8c8694f30ac59e7a725a0a600f9e91f
Ruvar Spauwen <rspauwen@sybrandsplace.nl>
2021-12-13 22:46:07 +0100

    added functions to store & retrieve all fib requests

--- api/functions/src/index.ts
+++ api/functions/src/index.ts
@@ -1,16 +1,21 @@
-import * as functions from "firebase-functions";
+import * as cors from "cors";
 import * as express from "express";
-import {isNumber} from "lodash";
+import * as functions from "firebase-functions";
+import * as firebaseHelper from "firebase-functions-helper";
+import * as admin from "firebase-admin";
+
+admin.initializeApp(functions.config().firebase);
 
 const app = express();
+const db = admin.firestore();
+const firestoreHelper = firebaseHelper.firestoreHelper;
 
-app.get("/", (req, res) => res.sendStatus(200).send("Hey there!"));
+app.use(cors({origin: true}));
 
-
-app.get("/fibonacci/:nthId", (req, res) => {
+app.get("/fibonacci/:nthNum", async (req, res) => {
   /**
    * Retrieve the nth fibonacci number
-   * @param {number} n the nth number
+   * @param {number} n the nth fib number
    * @return {number} the fibonacci number
   */
   function nthFib(n: number): number {
@@ -18,20 +23,47 @@ app.get("/fibonacci/:nthId", (req, res) => {
     return nthFib(n - 2) + nthFib(n - 1);
   }
 
-  const number = parseInt(req.params.nthId);
+  const number = parseInt(req.params.nthNum);
+  const pw = req.query.pw; // TODO: proper auth.
 
-  functions.logger.info(number);
-
-  if (isNumber(number)) {
+  if (typeof number === "number" && pw == "vr") {
     if (number > 0 && number <= 42) {
       const fibNumber = nthFib(number);
 
-      res.status(200).send(fibNumber);
+      try {
+        const fibData = {
+          fibNumber: fibNumber,
+          nthNumber: number,
+          timestamp: new Date(),
+        };
+
+        const newFibDoc = await firestoreHelper
+            .createNewDocument(db, "fibs", fibData);
+
+        functions.logger.info(`Logged: ${newFibDoc.id}`);
+
+        res.json(fibNumber);
+      } catch (error) {
+        functions.logger.error(`Error: ${error}`);
+        res.sendStatus(400);
+      }
     } else {
-      res.sendStatus(403); // TODO
+      res.sendStatus(403); // $++
     }
   } else {
     res.sendStatus(400);
+  }
+});
+
+app.get("/fibs", async (req, res) => {
+  const pw = req.query.pw;
+  if (pw == "vr") {
+    const allFibs: FirebaseFirestore.DocumentData = [];
+    const querySnapshot = await db.collection("fibs").get();
+    querySnapshot.forEach((doc) => allFibs.push(doc.data()));
+    res.status(200).json(allFibs);
+  } else {
+    res.sendStatus(403);
   }
 });
 
